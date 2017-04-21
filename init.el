cb039f9084e2271e0f9552311e5967e135031609
Marcus Olofsson <marcus.e.olofsson@gmail.com>
2017-04-21 11:21:46 +0200

    moved more towards a working rtags workflow

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; init.el --- Emacs configuration
+;; init.el --- Emacs configuration 
 
 ;; INSTALL PACKAGES
 ;; --------------------------------------
@@ -34,6 +34,7 @@
     company-flx
     company-irony
     company-irony-c-headers
+    company-rtags
     ;;ac-clang
     cmake-ide
     cmake-mode
@@ -44,13 +45,18 @@
     flycheck
     flycheck-irony
     flycheck-pyflakes
+    flycheck-rtags
     flymake-puppet
     irony
     helm
+    helm-c-yasnippet
+    helm-company
+    helm-ctest
+    helm-flycheck
+    helm-flx
     helm-projectile
     helm-swoop
-    helm-flx
-    helm-c-yasnippet
+    helm-rtags
     magit
     magit-filenotify
     material-theme
@@ -191,22 +197,85 @@
 
 ;; RTAGS CONFIGURATION
 ;; --------------------------------------
-; (require 'rtags)
-; (require 'company-rtags)
+(require 'rtags)
+(require 'company-rtags)
+
+(setq rtags-completions-enabled t)
+
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends 'company-rtags))
+
+(setq rtags-autostart-diagnostics t)
+(rtags-enable-standard-keybindings)
+
+
+(require 'company-irony)
+(require 'irony)
+(require 'helm-rtags)
+
+(setq rtags-use-helm t)
 ; (require 'company-clang)
 ; (require 'company-yasnippet)
-; (require 'company-irony-c-headers)
+(require 'company-irony-c-headers)
 ; (require 'flycheck-rtags)
-; ;; (require 'irony-mode)
+
 
 
 ; (setq cmake-ide-clang-flags-c '(getenv "LIBCLANG_CXXFLAGS"))
 ; (setq cmake-ide-clang-flags-c++ '(getenv "LIBCLANG_CXXFLAGS"))
 
 
-; (add-hook 'c++-mode-hook 'irony-mode)
-; (add-hook 'c-mode-hook 'irony-mode)
-; (add-hook 'objc-mode-hook 'irony-mode)
+
+;; Irony for C++ dev
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+
+
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+
+
+;; Company for C++ dev
+(setq company-backends (delete 'company-semantic company-backends))
+
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers company-irony)))
+
+(setq company-idle-delay 0)
+(define-key c-mode-map [(tab)] 'company-complete)
+(define-key c++-mode-map [(tab)] 'company-complete)
+
+;; Flycheck for c++ dev
+(add-hook 'c++-mode-hook 'flycheck-mode)
+(add-hook 'c-mode-hook 'flycheck-mode)
+(require 'flycheck-rtags)
+
+(defun my-flycheck-rtags-setup ()
+  (flycheck-select-checker 'rtags)
+  (setq-local flycheck-highlighting-mode nil)
+  (setq-local flycheck-check-syntax-automatically nil))
+
+(add-hook 'c-mode-comon-hook #'my-flycheck-rtags-setup)
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+(cmake-ide-setup)
+
+
+
 ; (add-hook 'c++-mode-hook 'company-mode)
 ; (add-hook 'c-mode-hook 'company-mode)
 
